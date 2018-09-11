b451ade5b24fc4195ebe3f7602f56a8aa66dd4a1
Eduardo Henao <eduardo.henao@outlook.com>
2018-09-12 00:03:39 +0200

    trying to make post work for UploadFile

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
@@ -18,6 +23,31 @@ namespace WebApplication1.Controllers
         {
             log("Home : Index");
             return View();
+        }
+
+        [HttpPost("UploadFiles")]
+        public async Task<IActionResult> Post(List<IFormFile> files)
+        {
+            long size = files.Sum(f => f.Length);
+
+            // full path to file in temp location
+            var filePath = Path.GetTempFileName();
+
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+
+            // process uploaded files
+            // Don't rely on or trust the FileName property without validation.
+
+            return Ok(new { count = files.Count, size, filePath });
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- WebApplication1/Controllers/UploadController.cs
+++ WebApplication1/Controllers/UploadController.cs
@@ -2,7 +2,9 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace WebApplication1.Controllers
@@ -16,25 +18,51 @@ namespace WebApplication1.Controllers
             _Factory = loggerFactory;
         }
 
-        [HttpPost]
-        public async Task<IActionResult> UploadFile(IFormFile file)
+        //[HttpPost("UploadFile")]
+        //public async Task<IActionResult> Post(List<IFormFile> files)
+        //{
+        //    if (files == null)
+        //    {
+        //        log("the file is empty.");
+        //    }
+
+        //    //get a new file path
+        //    //var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", Path.GetRandomFileName());
+
+        //    //using (var stream = new FileStream(path, FileMode.Create))
+        //    //{
+        //    //    await file.CopyToAsync(stream);
+        //    //}
+
+        //    log("Upload completed");
+        //    return RedirectToRoute("Home");
+        //}
+
+        [HttpPost("UploadFiles")]
+        public async Task<IActionResult> Post(List<IFormFile> files)
         {
-            if (file == null || file.Length == 0)
+            long size = files.Sum(f => f.Length);
+
+            // full path to file in temp location
+            var filePath = Path.GetTempFileName();
+
+            foreach (var formFile in files)
             {
-                log("the file is empty.");
+                if (formFile.Length > 0)
+                {
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
             }
 
-            //get a new file path
-            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", Path.GetRandomFileName());
+            // process uploaded files
+            // Don't rely on or trust the FileName property without validation.
 
-            using (var stream = new FileStream(path, FileMode.Create))
-            {
-                await file.CopyToAsync(stream);
-            }
-
-            log("Upload completed");
-            return RedirectToRoute("Home");
+            return Ok(new { count = files.Count, size, filePath });
         }
+
 
         public void log(string text)
         {
